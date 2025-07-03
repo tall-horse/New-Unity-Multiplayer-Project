6960e4a56cdbc36969069d168279950e3e264de0
tall-horse <dima.gamedev@pm.me>
2025-07-03 10:49:59 +0200

    testing network variables

--- NetcodeProject/Assets/Scripts/PlayerNetwork.cs
+++ NetcodeProject/Assets/Scripts/PlayerNetwork.cs
@@ -1,3 +1,4 @@
+using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -5,9 +6,45 @@ public class PlayerNetwork : NetworkBehaviour
 {
     [SerializeField] private float moveSpeed = 3f;
     private Vector3 moveDir = new Vector3(0, 0, 0);
+    private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(
+        new MyCustomData
+        {
+            _int = 56,
+            _bool = true,
+        }, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    public struct MyCustomData : INetworkSerializable
+    {
+        public int _int;
+        public bool _bool;
+        public FixedString128Bytes message;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+            serializer.SerializeValue(ref message);
+        }
+    }
+    public override void OnNetworkSpawn()
+    {
+        randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) =>
+        {
+            Debug.Log(OwnerClientId + "; " + newValue._int + "; " + newValue._bool + "; " + newValue.message);
+        };
+    }
     private void Update()
     {
         if (!IsOwner) return;
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            randomNumber.Value = new MyCustomData
+            {
+                _int = 10,
+                _bool = false,
+                message = "mesage lol",
+            };
+        }
 
         moveDir = new Vector3(0, 0, 0);
 
