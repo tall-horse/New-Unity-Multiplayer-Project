9438c3e470862ac39e946d93ea87e89b9dd69c60
tall-horse <dima.gamedev@pm.me>
2025-07-03 10:14:20 +0200

    added separate player movement

---
+++ NetcodeProject/Assets/Scripts/PlayerNetwork.cs
@@ -0,0 +1,21 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerNetwork : NetworkBehaviour
+{
+    [SerializeField] private float moveSpeed = 3f;
+    private Vector3 moveDir = new Vector3(0, 0, 0);
+    private void Update()
+    {
+        if (!IsOwner) return;
+
+        moveDir = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
+        if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
+        if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
+        if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
+
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+}
